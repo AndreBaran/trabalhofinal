67a771ff460a1b20f67e6ba282361898445e4a45
andrebaran <andrebarandossantos@yahoo.com.br>
2019-06-16 17:20:10 -0300

    navBar2

--- app/views/sales/_sale.json.jbuilder
+++ app/views/sales/_sale.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! sale, :id, :client, :value, :desc, :created_at, :updated_at
+json.extract! sale, :id, :client, :desc,:status, :created_at, :updated_at
 json.url sale_url(sale, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190615195107) do
+ActiveRecord::Schema.define(version: 20190616194739) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20190615195107) do
     t.text     "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
